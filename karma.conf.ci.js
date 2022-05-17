b2d59e1b5d99606ecb9ad1de731caf043afe64cf
Lucien Weller <lucien@wellernet.ch>
2022-05-17 23:06:57 +0200

    chore: configured ci for test execution

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -1,18 +1,20 @@
 const baseConfig = require('./karma.conf.js');
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
     baseConfig(config);
     config.set({
         plugins: [
             require('karma-jasmine'),
             require('karma-firefox-launcher'),
-            require('karma-phantomjs-launcher'),
+            require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
+            require('karma-coverage'),
             require('karma-junit-reporter'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
-        browsers: ['PhantomJS'],
+        browsers: ['ChromeHeadless'],
         singleRun: true
     });
 };
